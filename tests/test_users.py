b68210e4e5dc2091116b1a7437048aa2cb216551
VladiZ <vzaimov@outlook.com>
2020-05-06 14:57:10 +0300

    display user names when logged in

--- tests/test_users.py
+++ tests/test_users.py
@@ -176,6 +176,14 @@ class UsersTests(unittest.TestCase):
         for user in users:
             self.assertEqual(user.role, 'user')
 
+    def test_task_template_displays_logged_in_user_name(self):
+        self.register(
+            'Fletcher', 'fletcher@realpython.com', 'python101', 'python101'
+            )
+        self.login('Fletcher', 'python101')
+        response = self.app.get('tasks/', follow_redirects=True)
+        self.assertIn(b'Fletcher', response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
